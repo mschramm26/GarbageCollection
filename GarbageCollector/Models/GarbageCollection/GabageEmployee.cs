72eb8bccda82ef5e6027220546f76896d8936690
mschramm26 <melissa.schramm@gmail.com>
2018-02-21 10:16:34 -0600

    First commit

---
+++ GarbageCollector/Models/GarbageCollection/GabageEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GarbageCollector.Models.GarbageCollection
+{
+    namespace GarbageCollector.Models.Garbage
+    {
+        public class Employee
+        {
+            public int EmployeeId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int ZipCodeAssigned { get; set; }
+
+            public List<Customer> DayRoute { get; set; }
+
+            public Employee()
+            {
+
+            }
+
+
+
+        }
+    }
+}
---
+++ GarbageCollector/Models/GarbageCollection/GarbageCustomer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GarbageCollector.Models.GarbageCollection
+{
+    namespace GarbageCollector.Models.Garbage
+    {
+        public class Customer
+        {
+            public int CustomerId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Street { get; set; }
+            public string Street2 { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public int Zip { get; set; }
+            public int PhoneNumber { get; set; }
+            public string PickupDay { get; set; }
+            public string AmountOwed { get; set; }
+
+            public Customer()
+            {
+
+            }
+
+
+        }
+    }
+}
